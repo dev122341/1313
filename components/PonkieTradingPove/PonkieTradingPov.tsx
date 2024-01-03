03a8f0c8e2087596de0dd9accd1f965553ab4e0a
Riyad139 <riyad.ahmed1886@gmail.com>
2024-01-04 04:52:06 +0600

    1

--- components/PonkieTradingPove/PonkieTradingPov.tsx
+++ components/PonkieTradingPove/PonkieTradingPov.tsx
@@ -1,19 +1,16 @@
-import Link from "next/link";
+import Image from "next/image";
 
 export default function PonkieTradingPove() {
   return (
-    <div className="bg-pov relative bg-center bg-[url('/dog-bubble2.jpg')] bg-cover min-h-[28rem] py-4 md:py-36 px-16 md:px-24 bg-no-repeat md:min-h-[1200px] flex-1">
-      {/* <div className="animate-bounce">
-        <Link
-          target="_blank"
-          href={
-            "https://raydium.io/swap/?inputCurrency=sol&outputCurrency=6LRrSLtw3KpqAxDzbXJRq9yDr9fUTm6N3345kvb1BeLb&fixed=in"
-          }
-          className="bg-red uppercase   animate-bounce lg:mr-auto text-white py-3 px-5 text-3xl "
-        >
-          BUY Konko here
-        </Link>
-      </div> */}
+    <div className="relative">
+      <div className="flex  w-full  ">
+        <Image
+          src={"/dog-bubble2.jpg"}
+          width={1920}
+          height={1080}
+          alt="trading"
+        />
+      </div>
     </div>
   );
 }
--- components/TokenEnomics/TokenEnomics.tsx
+++ components/TokenEnomics/TokenEnomics.tsx
@@ -2,38 +2,42 @@ import Image from "next/image";
 import Link from "next/link";
 export default function TokenEnomics() {
   return (
-    <div className=" py-14 md:pt-32 pb-0  text-yellow-600 text-center flex flex-col items-center">
-      <h2 className=" text-4xl uppercase md:text-6xl font-bold">TOKENOMICS</h2>
-      <p className="text-white text-2xl max-w-[45rem]  mt-8 ">
-        {/* Anita Wynnn The Gambling Addicted Fairy Made 995,839,139.61 Tokens.{" "}
-        <span className="font-sans font-bold">{"She's"}</span> not a{" "}
-        <span className="text-red-600">greedy son of a Bitch</span> like her
-        Brother 👀 <span className="font-sans font-bold">{"She's"}</span> Ready
-        to Share them all */}
+    <div className=" py-10 md:pt-20 pb-0  text-yellow-600 text-center flex flex-col items-center">
+      <h2 className=" text-4xl uppercase md:text-8xl font-bold">TOKENOMICS</h2>
+      <p className="text-white text-2xl md:text-5xl max-w-[70rem]  mt-8 ">
         KONKO made 999.999.999 Tokens. And He {"doesn’t"} know what to do with
         Them 👀
       </p>
-      <div className="flex  w-full gap-10  mt-20">
-        <div className=" ">
-          <Image
-            className="animate-pulse"
-            src={"/dog-bubble1.jpg"}
-            width={1920}
-            height={1080}
-            alt="trading"
-          />
-        </div>
-      </div>
-      {/* <div className="flex w-full flex-col space-y-14 md:space-y-0 mt-14 md:flex-row  space-x-7 justify-center items-center">
+      <div className="flex w-full flex-col space-y-14  mt-14   space-x-7 justify-center items-center">
         <div className=" w-auto ">
-          <div className="w-full mb-5 px-14 text-2xl xl:text-4xl py-5 bg-yellow text-black">
+          <div className="w-full mb-5 px-20 text-2xl xl:text-6xl py-5 bg-yellow text-black">
             <p>TOTAL SUPPLY</p>
             <p>
               999.999.999 <span className="font-sans font-bold">$</span>KONKO
             </p>
           </div>
         </div>
-      </div> */}
+        <div className="animate-bounce">
+          <Link
+            target="_blank"
+            href={
+              "https://raydium.io/swap/?inputCurrency=sol&outputCurrency=6LRrSLtw3KpqAxDzbXJRq9yDr9fUTm6N3345kvb1BeLb&fixed=in"
+            }
+            className="bg-red uppercase   animate-bounce lg:mr-auto text-white py-3 px-5 text-3xl "
+          >
+            BUY Konko here
+          </Link>
+        </div>
+      </div>
+      <div className="flex  w-full gap-10  mt-20">
+        <Image
+          className="animate-pulse"
+          src={"/dog-bubble1.jpg"}
+          width={1920}
+          height={1080}
+          alt="trading"
+        />
+      </div>
     </div>
   );
 }
