113d5335dda6b19ea9bde2b8ff0c3674c0184a65
dev122341 <161851152+dev122341@users.noreply.github.com>
2024-03-01 10:51:39 -0500

    Update SocialLink2.tsx

--- components/SocialLink/SocialLink2.tsx
+++ components/SocialLink/SocialLink2.tsx
@@ -65,7 +65,7 @@ export default function SocialLink2() {
           </div>
           <div className="flex items-center  row-start-3 row-end-4 sm:flex-row flex-col mt-16 gap-7">
             <p className="text-5xl uppercase  lg:block hidden font-bold ">
-              <span className="font-sans">$</span>Konko
+              <span className="font-sans">$</span>BOSS
             </p>
             <div className="-rotate-3 md:rotate-3">
               <Link
