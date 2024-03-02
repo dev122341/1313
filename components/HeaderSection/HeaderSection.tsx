cd5c79f5a29090f76dd740b10c00092a5dc73761
dev122341 <161851152+dev122341@users.noreply.github.com>
2024-03-02 12:41:36 -0500

    Update HeaderSection.tsx

--- components/HeaderSection/HeaderSection.tsx
+++ components/HeaderSection/HeaderSection.tsx
@@ -77,7 +77,7 @@ export default function HeaderSection() {
                 >
                   <Image
                     className="w-16"
-                    src={"/ai.webp"}
+                    src={"/aii.webp"}
                     width={100}
                     height={100}
                     alt="logo"
